1b25195e4fc2e00fa67438f13c23661ad959998b
schuler <schuler.small.10@gmail.com>
2021-04-08 16:34:14 -0500

    working on follows button finished css

---
+++ express-project-starter/public/javascripts/follows.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const followButton = document.querySelector(".nav__follow-button");
+
+    followButton.addEventListener("click", async() => {
+        
+    });
+
+});
