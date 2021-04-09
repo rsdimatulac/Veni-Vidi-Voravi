d0c2f9814e899914c4be49d5457bb6e218191d5b
hye <davidckim22@gmail.com>
2021-04-09 04:24:25 -0400

    home page styling

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,30 @@ const { asyncHandler } = require('./utils');
 
 /* GET home page. */
 router.get('/', requireAuth, asyncHandler(async (req, res, next) => {
-  const stories = await db.Story.findAll({ 
+  const stories = await db.Story.findAll({
     order: [['createdAt', 'DESC']],
     include: db.User
   });
 
-  res.render('index', { stories, title: 'a/A Express Skeleton Home' });
+  const { userId } = req.session.auth
+
+  const followedUsers = await db.Follow.findAll({
+    where: {
+        userId
+    },
+    order: [['createdAt', 'DESC']]
+  });
+
+  const followedUserIds = followedUsers.map(el => el.followedUserId)
+
+  const follows = []
+
+  for (let i = 0; i < followedUserIds.length; i++) {
+    follows.push(await await db.User.findByPk(followedUserIds[i]))
+  }
+
+
+  res.render('index', { follows, stories });
 }));
 
 module.exports = router;
