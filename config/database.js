7369f8a867b4cdfc9ff2c262bcba90f1682fb48d
Renerose Dimatulac <rsdimatulac@gmail.com>
2021-04-08 19:18:28 -0500

    Edited DATABASE_URL

--- config/database.js
+++ config/database.js
@@ -13,7 +13,7 @@ module.exports = {
     logging: false
   },
   production: {
-    use_env_variable: 'postgres://bfdkazstkdtquu:b5056bed0ef2bc33c345b5e63615712486cb4fab9911504d943d70bfb36e7fcb@ec2-107-22-245-82.compute-1.amazonaws.com:5432/d99rqfknah1sr9',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
     dialectOptions: {
