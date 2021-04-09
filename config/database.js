8eb6c6ee7b911308ac47ba40fefaeb6d15af8af8
Renerose Dimatulac <rsdimatulac@gmail.com>
2021-04-08 19:11:52 -0500

    Added dialectOptions for Heroku

--- config/database.js
+++ config/database.js
@@ -15,6 +15,12 @@ module.exports = {
   production: {
     use_env_variable: 'postgres://bfdkazstkdtquu:b5056bed0ef2bc33c345b5e63615712486cb4fab9911504d943d70bfb36e7fcb@ec2-107-22-245-82.compute-1.amazonaws.com:5432/d99rqfknah1sr9',
     dialect: 'postgres',
-    seederStorage: 'sequelize'
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    }
   }
 };
